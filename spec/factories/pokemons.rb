4a92c66b097792b47559aac2a939412a74de2b88
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-11-23 11:57:18 -0700

    Add factories for search and pokemon

---
+++ spec/factories/pokemons.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :pokemon do
+    name { "MyString" }
+    weight { 1.5 }
+    height { 1.5 }
+    attack { 1 }
+    defense { 1 }
+    hp { 1 }
+    search { nil }
+  end
+end
