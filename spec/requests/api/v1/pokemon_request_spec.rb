52a0d395552550ce39c9c901d8b71a8f68e70f45
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-11-23 13:20:59 -0700

    Add test for not making another api call if the search and pokemon exists

--- spec/requests/api/v1/pokemon_request_spec.rb
+++ spec/requests/api/v1/pokemon_request_spec.rb
@@ -25,5 +25,31 @@ describe "Pokemon API" do
       expect(pokemon[:attack]).to eq(55)
       expect(pokemon[:hp]).to eq(35)
     end
+
+    it "wont return a pokemon if it doesnt exist" do
+      get '/api/v1/pokemon/notapokemon'
+      expect(response).to_not be_successful
+    end
+
+    it "wont make another api call if the search exists and pokemon exists" do
+      search = create(:search, name: "pikachu")
+      pokemon = Pokemon.create(
+        name: "Pikachu",
+        weight: 13.23,
+        height: 1.31,
+        defense: 40,
+        attack: 55,
+        hp: 55,
+        search_id: search.id
+      )
+
+      get '/api/v1/pokemon/pikachu'
+
+      expect(response).to be_successful
+      result = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+      expect(result[:name]).to eq("Pikachu")
+      expect(Search.last.id).to eq(search.id)
+      expect(Pokemon.last.id).to eq(pokemon.id)
+    end
   end
 end
